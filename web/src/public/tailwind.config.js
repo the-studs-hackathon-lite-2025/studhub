2026a1d279f8dfdac8f0144023e6e976d028eb0b
Joseph <contact@jooo.tech>
2025-03-08 15:43:44 +0100

    💩 Start "Kildebot"
    Co-authored-by: BusinessManBab <BusinessManBab@users.noreply.github.com>
    Co-authored-by: yosyas008 <yosyas008@users.noreply.github.com>

--- web/src/public/tailwind.config.js
+++ web/src/public/tailwind.config.js
@@ -7,6 +7,7 @@ tailwind.config = {
             },
             colors: {
                 'text': '#dcdcdc',
+                'primary': '#ffa31a',
                 'background': '#0a0a0a',
                 'highlight': '#0f0f0f',
                 'border': '#1b1b1b',
